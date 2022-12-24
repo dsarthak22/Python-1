4f030166d98ef784676ed443edb59bed6e8052fc
dsarthak22 <121254370+dsarthak22@users.noreply.github.com>
2022-12-25 00:35:45 +0530

    Add files via upload

---
+++ assignment1p2.py
@@ -0,0 +1,12 @@
+l1 =[12,-7,5,64,-14]
+for i in l1:
+    if i < 0:
+        l1.remove(i)
+
+print("Output: ",(l1))
+l2 =[12,14,-95,3]
+for i in l2:
+    if i < 0:
+        l2.remove(i)
+
+print("Output: ",(l2))
